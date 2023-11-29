17ba728757a12932ecabbf9ce1ff01bc50f57b18
Tanvir Rudro <rudro987@gmail.com>
2023-11-29 23:15:23 +0600

    token verified using jwt verify in middleware and admin verification api route created

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = (req, res, next) => {
+  if(!req.headers.authorization){
+    return res.status(401).send({message: 'Unauthorized access'});
+  }
+  const token = req.headers.authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'Unauthorized access'});
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     const db = client.db("elevroDB");
@@ -53,7 +68,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -89,6 +104,21 @@ async function run() {
     });
 
     // Admin related apis
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'Forbidden access'});
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user.role === 'admin';
+      }
+      res.send({ admin });
+    });
 
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
@@ -132,9 +162,7 @@ async function run() {
         }
       }
       
-      const result = await usersCollection.updateOne(filter, updatedDoc);
-      console.log(result);
-      
+      const result = await usersCollection.updateOne(filter, updatedDoc);      
       res.send(result);
     });
 
